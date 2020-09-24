fb6b03594e858696f3e86f8549b969e8f044b375
Harsh Patel <harshp97@gmail.com>
2020-09-23 21:25:35 -0400

    update

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var startButton = document.getElementById('startButton');
 var stopButton = document.getElementById('stopButton');
-var sound = document.getElementById('sound');
+var box = document.getElementById('box');
 
 var loop;
 
 startButton.onclick = () => {
+	startButton.disabled = 'disabled';
+	document.getElementById('text').innerHTML = 'Started';
 	loop = setInterval(isAvailable, 2000);
 };
 
 stopButton.onclick = () => {
+	startButton.removeAttribute('disabled');
+	document.getElementById('text').innerHTML = 'Stopped';
 	clearInterval(loop);
 };
 
@@ -31,11 +35,17 @@ function isAvailable() {
 	getJSON('https://api-prod.nvidia.com/direct-sales-shop/DR/products/en_us/USD/5438481700', function (err, data) {
 		try {
 			if (data.products.product[0].inventoryStatus.status !== 'PRODUCT_INVENTORY_OUT_OF_STOCK') {
+				box.className = 'color-box-green';
+				box.innerHTML = 'Found one';
 				clearInterval(loop);
 				var path = './alarm_beep.wav';
 				var snd = new Audio(path);
 				snd.play();
 				window.open('https://www.nvidia.com/en-us/geforce/graphics-cards/30-series/rtx-3080/', '_blank');
+			} else {
+				box.innerHTML = 'Nothing So Far';
+				box.className = 'color-box-red';
+				console.log('Nothing so far');
 			}
 		} catch (error) {
 			console.log('Error');
